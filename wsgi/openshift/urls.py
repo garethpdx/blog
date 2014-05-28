82435bdafcfba9dbf91bd70eb6bf8dd7cc2f3e7a
Gareth <the_valheru2@yahoo.com>
2014-05-27 19:46:55 -0700

    Adds handling for old-style urls.

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import RedirectView
+from django.conf.urls.defaults import handler404
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
+handler404 = 'openshift.views.go404'
 
 urlpatterns = patterns('',
                        url(r'^$', 'openshift.views.index', name='home'),
@@ -13,9 +17,14 @@ urlpatterns = patterns('',
                            'openshift.views.comment'),
                        url(r'^blog/comment/$',
                            'openshift.views.comment'),
+                       url(r'^admin/', include(admin.site.urls)),
+					   # Hard code 301-redirects until I figure out how to map all of the redirects.
+					   url(r'.*upgrading-sql-server-2012-from.html',RedirectView.as_view(url='/blog/10/')),
+					   url(r'.*connecting-to-sql-server-from-sikuli.html',RedirectView.as_view(url='/blog/8/')),
+					   
+					   )
 
-                       url(r'^admin/', include(admin.site.urls)),)
-
+					   
 # url(r'^openshift/', include('openshift.foo.urls')),
 # Uncomment the admin/doc line below to enable admin documentation:
 # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -1,7 +1,7 @@
 import os
 from django.shortcuts import render_to_response
 from django.template import Context, loader
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext
 from blogs.models import Post
 from blogs.models import Comment
@@ -9,6 +9,18 @@ from blogs.models import CommentForm
 
 def home(request):
     return render_to_response('home/home.html')
+
+def go404(request):
+	latest_blog_posts = Post.objects.filter(hidden=0)
+	t = loader.get_template('home/404.html')
+	c = Context({
+		'latest_blog_posts': latest_blog_posts[:10],
+		'older_blog_posts': latest_blog_posts[10:]
+		})
+	r = HttpResponse(t.render(c))
+	r.status_code=404
+	return r
+	
 
 def index(request):
 	latest_blog_posts = Post.objects.filter(hidden=0)
@@ -20,7 +32,12 @@ def index(request):
 	return HttpResponse(t.render(c))
 	
 def post(request, post_id):
-	post = Post.objects.get(pk=post_id)
+	try:
+		post = Post.objects.get(pk=post_id)
+	except Post.DoesNotExist:
+		raise Http404
+	#if not post:
+	#	raise Http404
 	all_comments = Comment.objects.filter(parent_id__exact=post_id, hidden=0)
 	t = loader.get_template('home/post.html')
 	c = Context({'post': post, 'all_comments': all_comments,'comment_form': CommentForm()})
