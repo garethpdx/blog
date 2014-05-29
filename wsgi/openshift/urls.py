3bad8e584380c0d769e65d9138c788d6c0d8a7e4
Gareth <the_valheru2@yahoo.com>
2014-05-28 22:28:12 -0700

    Adds robots.txt

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import RedirectView
 from django.conf.urls.defaults import handler404
+from django.http import HttpResponse
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+
 handler404 = 'openshift.views.go404'
+
 
 urlpatterns = patterns('',
                        url(r'^$', 'openshift.views.index', name='home'),
@@ -18,13 +21,15 @@ urlpatterns = patterns('',
                        url(r'^blog/comment/$',
                            'openshift.views.comment'),
                        url(r'^admin/', include(admin.site.urls)),
-					   # Hard code 301-redirects until I figure out how to map all of the redirects.
-					   url(r'.*upgrading-sql-server-2012-from.html',RedirectView.as_view(url='/blog/10/')),
-					   url(r'.*connecting-to-sql-server-from-sikuli.html',RedirectView.as_view(url='/blog/8/')),
-					   
-					   )
-
-					   
+                       # Hard code 301-redirects until I figure out
+                       # how to map all of the redirects.
+                       url(r'.*upgrading-sql-server-2012-from.html',
+                           RedirectView.as_view(url='/blog/10/')),
+                       url(r'.*connecting-to-sql-server-from-sikuli.html',
+                           RedirectView.as_view(url='/blog/8/')),
+                       url(r'^robots.txt$', lambda x: HttpResponse("User-agent: *" +
+                                                                   "\nDisallow: /", mimetype="text/plain")))
+	   
 # url(r'^openshift/', include('openshift.foo.urls')),
 # Uncomment the admin/doc line below to enable admin documentation:
 # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
