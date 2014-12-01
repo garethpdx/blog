86db35694f9bdb7bacded7e8a1891e3adeee9133
Gareth <gmackrill@mercycorps.org>
2014-11-30 17:24:43 -0800

    Adds tests for blog & measurement routes and requests
    Conflicts:
    	wsgi/openshift/blogs/tests.py

--- wsgi/openshift/blogs/tests.py
+++ wsgi/openshift/blogs/tests.py
@@ -1,14 +1,81 @@
-from django.core.urlresolvers import resolve
+import datetime
 
-from django.utils import unittest
-#  from django.test import TestCase
-from openshift.blogs.views import index
+from django.core.urlresolvers import resolve, Resolver404
+from django.test import TestCase
+from django.http import HttpRequest
+
+from openshift.views import go404
+from openshift.views import index
+
+from openshift.blogs.models import Post
+
+class SuccessfulRoutingTest(TestCase):
+    def setUp(self):
+        self.expected_function = None
+        self.urls = []
+
+    def test_resolution_of_page(self):
+        for url in self.urls:
+            found = resolve(url)
+            self.assertEqual(found.func, self.expected_function)
 
 
-class HomePageTest(unittest.TestCase):
-    def test_resolution_of_homepage(self):
-        found = resolve('/')
-        self.assertEqual(found.func, index)
+class HomepageRoutingTest(SuccessfulRoutingTest):
+    def setUp(self):
+        self.expected_function = index
+        self.urls = ['/']
 
 
+class NonRouteFailure(TestCase):
+    def setUp(self):
+        self.expected_function = go404
+        self.urls = ['/232352sadasae','/blogg/']
 
+    def test_routes(self):
+        for url in self.urls:
+            encountered_404 = False
+            try:
+                resolve(url)
+            except Resolver404:
+                encountered_404 = True
+            self.assertTrue(encountered_404)
+
+
+class RequestTest(TestCase):
+    def setUp(self):
+        self.request = HttpRequest()
+        self.urls = []
+
+    def test_page_http_status(self):
+        for url in self.urls:
+            found = resolve(url)
+            resp = self.get_response(found.func)
+            self.assertEqual(resp.status_code, 200)
+
+    def get_response(self, resolver):
+        return resolver(self.request)
+    
+
+class HomePageRequestTest(RequestTest):
+    def setUp(self):
+        self.request = HttpRequest()
+        self.urls = ['/']
+
+
+class BlogPostRequestTest(RequestTest):
+    @staticmethod
+    def create_example_post():
+        post = Post(content='test content',
+                         date=datetime.datetime(2014, 10, 07),
+                         title='test post title', blob='test_post',
+                         hidden=False)
+        post.save()
+        return post
+    
+    def setUp(self):
+        self.request = HttpRequest()
+        self.post = self.create_example_post()
+        self.urls = ['/2012/02/{0}/'.format(self.post.blob)]
+
+    def get_response(self, resolver):
+        return resolver(self.request, post_name=self.post.blob)
--- wsgi/openshift/measurement/tests.py
+++ wsgi/openshift/measurement/tests.py
@@ -1,16 +1,36 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+#  from django.utils import unittest
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+from django.core.urlresolvers import resolve
+
+from openshift.measurement.views import measure
+from openshift.measurement.models import Category
+
+class TestMeasurementUserRouting(TestCase):
+    TEST_USERNAME = 'tess'
+    TEST_PASSWORD = 'tuser'
+    def test_measurement_routing_for_new_user(self):
+        found = resolve('/measurement/{0}/'.format(self.TEST_USERNAME))
+        self.assertEqual(found.func, measure)
+
+    def setUp(self):
+        self.create_user()
+
+    def create_user(self):
+        User.objects.create_user(self.TEST_USERNAME,
+                                 password=self.TEST_PASSWORD)
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestMeasurementCategoryCreation(TestCase):
+
+    def test_category_count(self):
+        self.assertEqual(len(Category.objects.all()), 1)
+
+    def setUp(self):
+        self.create_category()
+
+    def create_category(self):
+         c = Category(name='test', hidden=False)
+         c.save()
+    
