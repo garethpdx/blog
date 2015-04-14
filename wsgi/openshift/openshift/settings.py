b02eecb3c64d8a19f5735bd8ab1482aac103e1d9
Gareth <gmackrill@mercycorps.org>
2015-04-13 19:04:47 -0700

    Update GA setting

--- wsgi/openshift/openshift/settings.py
+++ wsgi/openshift/openshift/settings.py
@@ -100,7 +100,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    #os.path.join(PROJECT_DIR, 'static/'),
+    os.path.join(PROJECT_DIR, 'static/'),
 )
 
 # List of finder classes that know how to find static files in
@@ -202,7 +202,6 @@ BLOG_CONFIGURATION = {
         'username': 'garethpdx',
         'widget_id': '534554833978531840'},
     'analytics': {
-        #'id': 'UA-38614627-4',
-        'id': '',
+        'id': 'UA-38614627-4',
         'domain': 'baselin.es'}
 }
