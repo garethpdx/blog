b52119d4cc8c73163717bbe40187ef53050ed08f
Gareth M <gareth@crumpet>
2015-12-01 21:37:47 -0800

    Update the blog list view to show when the post was created and whether or not it's published.

--- wsgi/openshift/openshift/blogs/admin.py
+++ wsgi/openshift/openshift/blogs/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from models import Post
 from models import Comment
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'hidden')
+
 admin.site.register(Comment)
